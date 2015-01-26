5bdee37e0ec6b6a0f07d308aa2d6a24c22cba6f7
jsivakum <student@epicodus.com>
2015-01-25 18:25:29 -0800

    To-do using ActiveRecord

---
+++ spec/task_spec.rb
@@ -0,0 +1,20 @@
+require("spec_helper")
+
+describe(Task) do
+
+  it ("tells which list it belongs to") do
+    list = List.create({:name => "list"})
+    task = Task.create({:description => "task", :list_id => list.id})
+    expect(task.list).to eq list
+  end
+
+  describe(".not_done") do
+    it("retuns the not done tasks") do
+        not_done_task1 = Task.create(:description => "gotta do it", :done => false)
+        not_done_task2 = Task.create(:description => "gotta do it too", :done =>false)
+        not_done_tasks = [not_done_task1, not_done_task2]
+        done_task = Task.create({:description => "done task", :done => true})
+        expect(Task.not_done).to eq not_done_tasks
+    end
+  end
+end
